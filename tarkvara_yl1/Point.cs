f82f083e58d28f255e6995243dc60267f49405e9
Mark Matteus Murru <mamurr@ttu.ee>
2020-09-08 23:45:49 +0300

    Add project files.

---
+++ tarkvara_yl1/Point.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace tarkvara_yl1
+{
+    public class Point
+    {
+        private double x1;
+        private double y1;
+
+        public Point(double x, double y)
+        {
+            x1 = x;
+            y1 = y;
+                
+        }
+        public double GetX()
+        {
+            return x1;
+        }
+        public void SetX(double x)
+        {
+            x1 = x;
+        }
+        public double GetY()
+        {
+            return y1;
+        }
+        public void SetY(double y)
+        {
+            y1 = y;
+        }
+
+        public double GetRho()
+        {
+            return Math.Sqrt(Math.Pow(x1, 2) + Math.Pow(y1, 2));
+        }
+
+        public double GetTheta()
+        {
+            return Math.Atan2(y1, x1);
+
+        }
+
+        public double GetDistance(Point p1, Point p2)
+        {
+            return Math.Sqrt(Math.Pow(p1.GetX() - p2.GetX(), p1.GetY() - p2.GetY()));
+        }
+
+        public Point VectorTo(Point p1, Point p2)
+        {
+            return new Point(p2.GetX() - p1.GetX(), p2.GetY() - p1.GetY());
+        }
+
+        public double Distance(Point p1, Point p2)
+        {
+            return p1.VectorTo(p1, p2).GetRho();
+        }
+
+        public void Translate(Point p1, double dx, double dy)
+        {
+            p1.x1 += dx;
+            p1.y1 += dy;
+        }
+
+        public void Scale(Point p1, double factor)
+        {
+            p1.x1 *= factor;
+            p1.y1 *= factor;
+        }
+
+        public Point CentreRotate(Point p1, double angle)
+        {
+            double tempx = p1.GetRho() * Math.Cos(p1.GetTheta() + angle);
+            double tempy = p1.GetRho() * Math.Sin(p1.GetTheta() + angle);
+            p1.x1 = tempx;
+            p1.y1 = tempy;
+            return p1;
+        }
+
+        public void Rotate(Point p, double angle)
+        {
+            p.Translate(p,-(p.x1), -(p.y1));
+            p.CentreRotate(p, angle);
+            p.Translate(p,p.x1,p.y1);
+                
+
+        }
+    }
+
+}
---
+++ tarkvara_yl1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace tarkvara_yl1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(10, 20);
+            Point p2 = new Point(-20, 60);
+            double distance = p1.Distance(p1, p2);
+            Console.WriteLine("1. yl vastus on : {0}", distance);
+            Point p3 = new Point(15, 0);
+            p3.CentreRotate(p3, Math.PI / 3);
+            Console.WriteLine("2. yl vastus on : {0}, {1}", p3.GetX(), p3.GetY());
+        }
+
+    }
+}
